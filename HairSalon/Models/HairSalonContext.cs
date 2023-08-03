e2f27049b655d79136f3613cfcd970179c19e845
Thomas Bakken <tbakken14@gmail.com>
2023-08-02 22:12:26 -0700

    Add models to db context

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,6 +4,8 @@ namespace HairSalon.Models
 {
     public class HairSalonContext : DbContext
     {
+        DbSet<Client> Clients { get; set; }
+        DbSet<Stylist> Stylists { get; set; }
         public HairSalonContext(DbContextOptions options) : base(options) { }
     }
 }
